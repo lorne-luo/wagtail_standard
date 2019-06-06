f96b40ab5dcbb01ded9a0e67335d1b6257dee249
Lorne <lorne.luo@butterfly.com.au>
2019-06-06 13:32:52 +1000

    custom template page

--- core/wagtail/page.py
+++ core/wagtail/page.py
@@ -1,3 +1,7 @@
+import os
+
+from django.db import models
+from django.utils.functional import lazy
 from wagtail.core.fields import StreamField
 from wagtail.core.models import Page
 
@@ -9,3 +13,29 @@ class StreamPage(Page):
 
     class Meta:
         abstract = True
+
+
+class TemplatePage(Page):
+    custom_template = models.CharField(max_length=250, blank=True, null=False, choices=())
+
+    class Meta:
+        abstract = True
+
+    def __init__(self, *args, **kwargs):
+        super(TemplatePage, self).__init__(*args, **kwargs)
+        self._meta.get_field('custom_template').choices = lazy(self.custom_template_choices, list)()
+
+    def get_template(self, request, *args, **kwargs):
+        if request.is_ajax() and self.ajax_template:
+            return self.ajax_template
+        return self.custom_template or super(TemplatePage, self).get_template(request, *args, **kwargs)
+
+    def custom_template_choices(self):
+        choices = []
+        app_label = self._meta.app_label
+        path = os.path.join(os.path.dirname(__file__), 'templates', app_label)
+        for file in os.listdir(path):
+            if file.endswith(".html"):
+                template = "%s/%s" % (app_label, file)
+                choices.append((template, template))
+        return choices
